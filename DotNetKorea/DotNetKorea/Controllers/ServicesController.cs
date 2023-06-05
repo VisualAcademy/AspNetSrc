4465bec06926de51eb28be25de51cc2c65a9f08b
Park YongJune <redplus@redplus.net>
2023-06-06 06:19:39 +0900

    DotNetKorea Solution Add

---
+++ DotNetKorea/DotNetKorea/Controllers/ServicesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DotNetKorea.Controllers
+{
+    public class ServicesController : ApiController
+    {
+        // GET: api/Services
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Services/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Services
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Services/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Services/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
